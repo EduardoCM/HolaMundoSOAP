11f3f3e2df69197dd0a93c2fd51d81b11d22c6b9
David <david.osornio99@gmail.com>
2018-02-10 14:54:48 -0600

    Actualizacion cuenta

--- src/main/java/org/tesji/davidosornio/DepositoEfectivoServiceImpl.java
+++ src/main/java/org/tesji/davidosornio/DepositoEfectivoServiceImpl.java
@@ -12,11 +12,14 @@ public class DepositoEfectivoServiceImpl implements DepositoefectivoService{
     @Override
     public String Deposito(String cuenta, int cantidad) {
         String deposito = "";
-        
-        if (cuenta.equals("12345") && cantidad>0){
+        if (cantidad > 0){
+        if (cuenta.equals("12345")){
+            deposito += "Deposito a la cuenta " + cuenta + " la cantidad de " + cantidad;
+        }else if (cuenta.equals("123456789")){
             deposito += "Deposito a la cuenta " + cuenta + " la cantidad de " + cantidad;
         }else{
             deposito += "No exite el numero de cuenta o no es valido para este banco";
+        }
         }
         return deposito;
     }
